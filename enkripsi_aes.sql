b8688defaebd608fc7a58eb789899d877b74593a
Reza Satria <reza.shinra@gmail.com>
2015-10-27 10:56:24 +0700

    commit ketiga

---
+++ enkripsi_aes.sql
@@ -0,0 +1,48 @@
+﻿# Host: localhost  (Version: 5.6.20)
+# Date: 2015-10-27 10:55:39
+# Generator: MySQL-Front 5.3  (Build 4.205)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "dokumen"
+#
+
+DROP TABLE IF EXISTS `dokumen`;
+CREATE TABLE `dokumen` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id_user` int(5) DEFAULT NULL,
+  `nama_dokumen` varchar(80) DEFAULT NULL,
+  `dokumen_id` varchar(80) DEFAULT NULL,
+  `nama_enkrip` varchar(255) DEFAULT NULL,
+  `tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+#
+# Data for table "dokumen"
+#
+
+/*!40000 ALTER TABLE `dokumen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dokumen` ENABLE KEYS */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+#
+# Data for table "user"
+#
+
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','admin@admin.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),(2,'reza','reza.shinra@gmail.com','8bd8b63d5cc9b9605c91427bd508db1b0e0d3d7c9c205054e57c6561334af295');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
